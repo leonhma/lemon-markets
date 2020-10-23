da0070dd13a4e8feb375ccf021dca53aff597f72
Marcel <marcel.katenhusen@me.com>
2020-10-23 13:54:45 +0200

    set pypi name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lemon-markets-sdk",
+    name="lemon_markets",
     version="0.0.1",
     author="Marcel Katenhusen",
     author_email="marcel@lemon.markets",
