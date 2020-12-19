f24d88234e64df01d36e4d20f0279e5a71b01462
leonhma <leonhard.masche@gmail.com>
2020-12-19 14:37:05 +0000

    Updating page version (triggered by push)

--- lemon_markets/__init__.py
+++
@@ -1,2 +0,0 @@
-default_rest_api_url: str = "https://api.lemon.markets/rest/v1/"
-default_stream_api_url: str = "https://api.lemon.markets/streams2/v1/"
--- lemon_markets/settings.py
+++
@@ -1,2 +0,0 @@
-DEFAULT_REST_API_URL: str = "https://api.lemon.markets/rest/v1/"
-DEFAULT_STREAM_API_URL: str = "wss://api.lemon.markets/streams/v1/"
